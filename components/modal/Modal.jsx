1d6619afbf6111f9c7d3aa6a81f4459588b876d0
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-06-05 19:14:20 +0600

    added animation to modal

--- components/modal/Modal.jsx
+++ components/modal/Modal.jsx
@@ -1,19 +1,49 @@
-import React from 'react'
+import { motion } from 'framer-motion'
 import { IoMdClose } from 'react-icons/io'
 import './modal.css'
 
 const Modal = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null
 
+  const backdropVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+    exit: { opacity: 0 },
+  }
+
+  const modalVariants = {
+    hidden: { opacity: 0, y: '-50%' },
+    visible: { opacity: 1, y: '0%' },
+    exit: { opacity: 0, y: '-50%' },
+  }
+
+  const transition = {
+    duration: 0.3,
+    ease: 'easeInOut',
+  }
+
   return (
-    <div className="modal">
-      <div className="modal-content">
+    <motion.div
+      className="modal"
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+      variants={backdropVariants}
+      transition={transition}
+      onClick={onClose}
+    >
+      <motion.div
+        className="modal-content"
+        variants={modalVariants}
+        transition={transition}
+        onClick={(e) => e.stopPropagation()}
+      >
         <button className="close-button" onClick={onClose}>
           <IoMdClose />
         </button>
         {children}
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   )
 }
 

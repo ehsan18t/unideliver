fe4f7249ffde7635ea8448eacb4d62dca0a0483a
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-07-29 20:45:39 +0600

    Update ShopBanner.jsx

--- components/shopList/ShopBanner.jsx
+++ components/shopList/ShopBanner.jsx
@@ -1,7 +1,7 @@
 const ShopBanner = ({ shop }) => {
   return (
-    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-1/3 w-full flex gap-3 justify-start items-center">
-      <img src={shop.pictureURL} className="h-72 w-96" alt="" />
+    <div className="shadow-md bg-gradient-to-r from-cyan-500 to-blue-500 h-1/3 w-full flex gap-3 justify-start items-center">
+      <img src={shop.pictureURL} className="p-4 rounded-md h-72 w-96" alt="" />
       <div className="w-full flex flex-col gap-3 justify-center items-center">
         <p className="text-white text-6xl font-semibold">{shop.shopName}</p>
         <p className="text-gray-100 text-3xl">{shop.location}</p>

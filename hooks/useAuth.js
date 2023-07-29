6074aa3af6201420a1c806060731d5ef63019d83
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-07-29 14:36:48 +0600

    added custom useAuth hook

---
+++ hooks/useAuth.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react'
+import { auth } from '@/config/firebase' // Import your Firebase configuration
+
+const useAuth = () => {
+  const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        setUser(authUser)
+      } else {
+        setUser(null)
+      }
+      setLoading(false)
+    })
+
+    return () => unsubscribe()
+  }, [])
+
+  return { user, loading }
+}
+
+export { useAuth }

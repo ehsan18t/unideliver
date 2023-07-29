8476d0ab79697e753db5360be10ba54f0d4b047b
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-07-29 17:02:51 +0600

    fixed the shop info is not showing

--- app/shop/[shopId]/page.jsx
+++ app/shop/[shopId]/page.jsx
@@ -1,48 +1,24 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import { collection, doc, getDoc } from 'firebase/firestore'
+import { collection, onSnapshot, doc } from 'firebase/firestore'
 import { db } from '@/config/firebase'
-import { usePathname } from 'next/navigation'
+import { useParams } from 'next/navigation'
 
 export default function ShopPage() {
-  const shopId = usePathname().split('/')[3]
+  const params = useParams()
+  const shopId = params.shopId
   const [shop, setShop] = useState(null)
-  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    const fetchShop = async () => {
-      try {
-        const shopDocRef = doc(collection(db, 'shops'), shopId)
-        const shopDocSnap = await getDoc(shopDocRef)
-
-        if (shopDocSnap.exists()) {
-          setShop({ id: shopDocSnap.id, ...shopDocSnap.data() })
-        } else {
-          console.log('Shop not found')
-          // Redirect to a 404 page or handle accordingly
-        }
-      } catch (error) {
-        console.error('Error fetching shop:', error)
-        // Handle error
-      } finally {
-        setLoading(false)
-      }
-    }
-
-    if (shopId) {
-      fetchShop()
-    }
+    const unsubscribe = onSnapshot(doc(db, 'shops', shopId), (doc) => {
+      setShop(doc.data())
+    })
+    return () => unsubscribe()
   }, [shopId])
 
-  if (loading) {
-    // Handle loading state
-    return null
-  }
-
   if (!shop) {
-    // Handle shop not found state
-    return <p>Shop not found</p>
+    return <div>Loading...</div>
   }
 
   return (

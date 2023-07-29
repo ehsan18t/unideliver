34717f8142dc6b04af9578c0c37c71bb854d928a
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-07-29 21:28:00 +0600

    added working Add Item form

--- app/shop/[shopId]/page.jsx
+++ app/shop/[shopId]/page.jsx
@@ -1,22 +1,46 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import { collection, onSnapshot, doc } from 'firebase/firestore'
+import { collection, onSnapshot, doc, query, where } from 'firebase/firestore'
 import { db } from '@/config/firebase'
 import { useParams } from 'next/navigation'
 import ShopBanner from '@/components/shopList/ShopBanner'
 import Loading from '@/components/common/Loading'
+import AddItemForm from '@/components/item/AddItemForm'
+import { useAuth } from '@/hooks/useAuth'
+import Modal from '@/components/modal/Modal'
 
 export default function ShopPage() {
   const params = useParams()
   const shopId = params.shopId
   const [shop, setShop] = useState(null)
+  const [items, setItems] = useState([])
+  const [modalOpen, setModalOpen] = useState(false)
+  const { user } = useAuth()
 
   useEffect(() => {
     const unsubscribe = onSnapshot(doc(db, 'shops', shopId), (doc) => {
       setShop(doc.data())
     })
     return () => unsubscribe()
+  }, [shopId])
+
+  useEffect(() => {
+    const itemsQuery = query(
+      collection(db, 'items'),
+      where('shopId', '==', shopId),
+    )
+    const unsubscribeItems = onSnapshot(itemsQuery, (snapshot) => {
+      const itemList = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }))
+      setItems(itemList)
+    })
+
+    return () => {
+      unsubscribeItems()
+    }
   }, [shopId])
 
   if (!shop) {
@@ -26,6 +50,24 @@ export default function ShopPage() {
   return (
     <div>
       <ShopBanner shop={shop} />
+      {user && user.isAdmin && (
+        <button onClick={() => setModalOpen(true)}>Add Item</button>
+      )}
+      <div>
+        {items &&
+          items.map((item) => (
+            <div key={item.id}>
+              <h2>{item.name}</h2>
+              <p>{item.description}</p>
+              <p>{item.price}</p>
+
+              <button>Add to cart</button>
+            </div>
+          ))}
+      </div>
+      <Modal isOpen={modalOpen} onClose={() => setModalOpen(false)}>
+        <AddItemForm onClose={() => setModalOpen(false)} shopId={shopId} />
+      </Modal>
     </div>
   )
 }
---
+++ components/item/AddItemForm.jsx
@@ -0,0 +1,122 @@
+import React, { useState } from 'react'
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
+import { collection, addDoc } from 'firebase/firestore'
+import { db, storage } from '@/config/firebase'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+const AddItemForm = ({ onClose, shopId }) => {
+  const [itemName, setItemName] = useState('')
+  const [description, setLocation] = useState('')
+  const [price, setPrice] = useState(0.0)
+  const [picture, setPicture] = useState(null)
+
+  const handleAddItem = async () => {
+    try {
+      if (itemName === '' || description === '' || picture === null) {
+        toast.error('Please fill in all the required fields.', {
+          position: 'top-center',
+          autoClose: 2000,
+          theme: 'colored',
+        })
+        return
+      }
+
+      // Convert the picture to a data URL string
+      const reader = new FileReader()
+      reader.onload = async (event) => {
+        const dataURL = event.target.result
+
+        // Upload the picture to Firebase storage
+        const storageRef = ref(storage, `itemPictures/${picture.name}`)
+        await uploadBytes(storageRef, picture)
+
+        // Get the download URL of the uploaded picture
+        const pictureURL = await getDownloadURL(storageRef)
+
+        // Add the item to the 'items' collection in Firestore
+        await addDoc(collection(db, 'items'), {
+          itemName,
+          description,
+          price,
+          pictureURL,
+          shopId,
+        })
+
+        setItemName('')
+        setLocation('')
+        setPicture(null)
+        onClose() // Close the modal
+      }
+
+      reader.readAsDataURL(picture)
+    } catch (error) {
+      console.error('Error adding item:', error)
+    }
+  }
+
+  const handlePictureUpload = (event) => {
+    const file = event.target.files[0]
+    setPicture(file)
+  }
+
+  return (
+    <div>
+      <h3 className="text-xl font-bold mb-4">Add Item</h3>
+      <div className="modal-content">
+        <label htmlFor="itemName" className="block mb-2">
+          Item Name:
+        </label>
+        <input
+          type="text"
+          id="itemName"
+          value={itemName}
+          onChange={(e) => setItemName(e.target.value)}
+          className="w-full px-2 py-1 border rounded mb-2"
+        />
+
+        <label htmlFor="description" className="block mb-2">
+          Description:
+        </label>
+        <input
+          type="text"
+          id="description"
+          value={description}
+          onChange={(e) => setLocation(e.target.value)}
+          className="w-full px-2 py-1 border rounded mb-2"
+        />
+
+        <label htmlFor="price" className="block mb-2">
+          Price:
+        </label>
+        <input
+          type="number"
+          id="price"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+          className="w-full px-2 py-1 border rounded mb-2"
+        />
+
+        <label htmlFor="picture" className="block mb-2">
+          Picture:
+        </label>
+        <input
+          type="file"
+          id="picture"
+          accept="image/*"
+          onChange={handlePictureUpload}
+          className="mb-2"
+        />
+
+        <button
+          className="add-item-button bg-blue-500 text-white rounded px-4 py-2"
+          onClick={handleAddItem}
+        >
+          Add Item
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default AddItemForm

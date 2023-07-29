7e5e094a32865a8ffe6cf8f9d1e18d70451f0568
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-07-29 15:03:31 +0600

    added admin route to nav

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,13 @@
+'use client'
 import { AiOutlineHome, AiOutlineShop } from 'react-icons/ai'
+import { RiAdminLine } from 'react-icons/ri'
 import Navbar from '@/components/nav/Navbar'
 import { NavItem } from '@/components/nav/NavItem'
 import './globals.css'
+import { useEffect, useState } from 'react'
+import { useAuth } from '@/hooks/useAuth'
+import { onSnapshot, doc } from 'firebase/firestore'
+import { db } from '@/config/firebase'
 
 export const metadata = {
   title: 'Home Page',
@@ -9,12 +15,35 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
+  const { user, loading } = useAuth()
+  const [isAdmin, setIsAdmin] = useState(false)
+
+  useEffect(() => {
+    if (!loading && user) {
+      const userRef = doc(db, 'users', user.uid)
+      const unsubscribe = onSnapshot(userRef, (doc) => {
+        if (doc.exists()) {
+          const userData = doc.data()
+          setIsAdmin(userData.isAdmin)
+        }
+      })
+
+      return () => {
+        // Cleanup the listener when the component unmounts
+        unsubscribe()
+      }
+    }
+  }, [user, loading])
+
   return (
     <html lang="en">
       <body>
         <Navbar>
-          <NavItem to="#" name="Home" icon={AiOutlineHome} />
+          <NavItem to="/" name="Home" icon={AiOutlineHome} />
           <NavItem to="#" name="Shop" icon={AiOutlineShop} />
+          {user && isAdmin && (
+            <NavItem to="/admin" name="Admin" icon={RiAdminLine} />
+          )}
         </Navbar>
         {children}
       </body>

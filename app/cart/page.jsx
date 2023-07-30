e13526d65eefab365db398f6eac8979de546e4b4
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-07-30 08:57:07 +0600

    completed cart page

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react'
 import { collection, onSnapshot } from 'firebase/firestore'
 import { db, auth } from '@/config/firebase'
 import CartItem from '@/components/cart/CartItem'
+import useItemDeletion from '@/hooks/useItemDeletion'
 
 const fetchUserData = async () => {
   return new Promise((resolve) => {
@@ -40,6 +41,7 @@ const CartItems = () => {
   const [loading, setLoading] = useState(true)
   const [user, setUser] = useState(null)
   const [cartItems, setCartItems] = useState([])
+  const { delLoading, error, deleteItem } = useItemDeletion()
 
   useEffect(() => {
     const fu = async () => {
@@ -70,11 +72,19 @@ const CartItems = () => {
     return <p>Please log in to view cart items.</p>
   }
 
+  const handleDeleteCartItem = async (item) => {
+    deleteItem('cart', item.id)
+  }
+
   return (
-    <div>
+    <div className="m-4 rounded-lg bg-white p-4 shadow-md">
       {cartItems.length > 0 ? (
         cartItems.map((cartItem) => (
-          <CartItem key={cartItem.id} item={cartItem.item} />
+          <CartItem
+            key={cartItem.id}
+            cartItem={cartItem}
+            onDelete={() => handleDeleteCartItem(cartItem)}
+          />
         ))
       ) : (
         <p>No cart items found.</p>
--- components/cart/CartItem.jsx
+++ components/cart/CartItem.jsx
@@ -1,15 +1,42 @@
-import React from 'react'
+import { RiDeleteBin5Line } from 'react-icons/ri'
 
-const CartItem = ({ item }) => {
+const CartItem = ({ cartItem, onDelete }) => {
+  const item = cartItem.item
   if (!item) {
     return null // If item is null, don't render anything
   }
 
-  const { itemName, id } = item
+  const handleDeleteClick = () => {
+    onDelete(cartItem)
+  }
+
   return (
     <div>
-      <h3>Item: {itemName}</h3>
-      <p>ID: {id}</p>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <img
+            src={item.pictureURL}
+            alt={item.name}
+            className="w-16 h-16 rounded-lg"
+          />
+          <div>
+            <p className="text-lg font-semibold">{item.itemName}</p>
+            <p className="text-gray-500">
+              {item.price} + {item.deliveryCharge}
+            </p>
+
+            <div className="flex gap-2">
+              <button className="bg-gray-200 rounded-lg px-2 py-1">-</button>
+              <p>1</p>
+              <button className="bg-gray-200 rounded-lg px-2 py-1">+</button>
+            </div>
+          </div>
+        </div>
+        <RiDeleteBin5Line
+          className="shop-item-del-button"
+          onClick={() => handleDeleteClick(item)}
+        />
+      </div>
     </div>
   )
 }

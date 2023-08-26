d697f1ef683f81fb8c22c1af9188e463eee4e52e
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-08-26 10:28:05 +0600

    fixed cart live refresh

--- components/cart/CartPane.jsx
+++ components/cart/CartPane.jsx
@@ -1,78 +1,15 @@
 'use client'
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef } from 'react'
 import { BsCart4 } from 'react-icons/bs'
-import { collection, onSnapshot } from 'firebase/firestore'
-import { db, auth } from '@/config/firebase'
 import CartItem from '@/components/cart/CartItem'
 import useItemDeletion from '@/hooks/useItemDeletion'
-
-const fetchUserData = async () => {
-  return new Promise((resolve) => {
-    const unsubscribeUser = auth.onAuthStateChanged((user) => {
-      resolve(user)
-      unsubscribeUser()
-    })
-  })
-}
-
-const fetchCartItems = async (userId) => {
-  return new Promise((resolve) => {
-    const shopsCollection = collection(db, 'cart')
-    const unsubscribe = onSnapshot(shopsCollection, (snapshot) => {
-      const shopList = snapshot.docs
-        .map((doc) => {
-          const data = doc.data()
-          if (data.uid === userId) {
-            return {
-              id: doc.id,
-              ...data,
-            }
-          }
-          return null
-        })
-        .filter((item) => item !== null)
-      resolve(shopList)
-      unsubscribe()
-    })
-  })
-}
+import { useFetchLive } from '@/hooks'
 
 const CartPane = ({ children }) => {
   const [isSidebarOpen, setSidebarOpen] = useState(false)
   const hamburgerButtonRef = useRef(null)
-  const [loading, setLoading] = useState(true)
-  const [user, setUser] = useState(null)
-  const [cartItems, setCartItems] = useState([])
   const { delLoading, error, deleteItem } = useItemDeletion()
-
-  useEffect(() => {
-    const fu = async () => {
-      const user = await fetchUserData()
-      setUser(user)
-      setLoading(false)
-    }
-    fu()
-  }, [])
-
-  useEffect(() => {
-    if (user) {
-      const fi = async () => {
-        const shopList = await fetchCartItems(user.uid)
-        setCartItems(shopList)
-      }
-      fi()
-    } else {
-      setCartItems([])
-    }
-  }, [user])
-
-  if (loading) {
-    return <p>Loading...</p>
-  }
-
-  if (!user) {
-    return <p>Please log in to view cart items.</p>
-  }
+  const { data, loading: isDataLoading } = useFetchLive('cart')
 
   const handleDeleteCartItem = async (item) => {
     deleteItem('cart', item.id)
@@ -97,8 +34,8 @@ const CartPane = ({ children }) => {
             Cart
           </div>
           <div className="mt-4">
-            {cartItems.length > 0 ? (
-              cartItems.map((cartItem) => (
+            {data.length > 0 ? (
+              data.map((cartItem) => (
                 <CartItem
                   key={cartItem.id}
                   cartItem={cartItem}
---
+++ hooks/index.js
@@ -0,0 +1,1 @@
+export { default as useFetchLive } from './useFetchLive'

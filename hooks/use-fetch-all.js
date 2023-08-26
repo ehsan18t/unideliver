2c26db19c4158b25be9883db7ebe5520655074ad
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-08-26 22:49:44 +0600

    added new hook to fetch whole table

---
+++ hooks/use-fetch-all.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+import { collection, onSnapshot } from 'firebase/firestore'
+import { db } from '@/config/firebase' // Update this import to match your Firebase setup
+
+const useFetchAll = (tableName) => {
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const query = collection(db, tableName)
+      const unsubscribe = onSnapshot(query, (snapshot) => {
+        const newData = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }))
+        setData(newData)
+        setLoading(false)
+      })
+
+      return () => unsubscribe()
+    }
+
+    fetchData()
+  }, [tableName])
+
+  return { data, loading }
+}
+
+export default useFetchAll

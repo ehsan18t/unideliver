c70c6cec1a1a352eecdf95a6c6491afa7d6cca5c
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-08-26 20:14:35 +0600

    added order item design

--- components/order/Order.jsx
+++ components/order/Order.jsx
@@ -1,13 +1,40 @@
 import React from 'react'
+import dayjs from 'dayjs'
+import Link from 'next/link'
+
+const status = {
+  1: 'Pending',
+  2: 'Confirmed',
+  3: 'Delivered',
+  4: 'Cancelled',
+}
 
 const Order = ({ order }) => {
   return (
-    <div>
-      <h2>Order</h2>
-      <p>Order ID: {order.id}</p>
-      <p>Order Status: {order.status}</p>
-      <p>Order Total: {order.total}</p>
-      <p>Order Date: {order.date}</p>
+    <div className="border-b-[1px] p-3">
+      <Link href={`/orders/${order.id}`}>
+        <div className="space-y-2">
+          <div className="flex justify-between items-center">
+            <h3 className="text-xl font-semibold">Order #{order.id}</h3>
+            <p className="text-sm text-gray-500">
+              {dayjs(order.date).format('DD-MMM-YYYY hh:mm A')}
+            </p>
+          </div>
+          <div className="flex justify-between items-center">
+            <p>
+              <span className="font-semibold">Location:</span> {order.location}
+            </p>
+            <p>{status[order.status]}</p>
+          </div>
+          <div className="flex justify-between items-center">
+            <p>
+              <span className="font-semibold">Item Quantity:</span>{' '}
+              {order.items.length}
+            </p>
+            <p>Total Amount (TK): {order.total}</p>
+          </div>
+        </div>
+      </Link>
     </div>
   )
 }

f5c1260a310a38686b25ae84d038b918be1eff92
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-08-26 11:11:35 +0600

    updated button green color

--- components/common/Button.jsx
+++ components/common/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ color = 'blue', onClick, className, children }) => {
   const colorClasses = {
     blue: 'bg-blue-500 hover:bg-blue-600 text-white',
     red: 'bg-red-500 hover:bg-red-600 text-white',
-    green: 'bg-green-500 hover:bg-green-600 text-white',
+    green: 'bg-green-600 hover:bg-green-700 text-white',
   }
 
   return (

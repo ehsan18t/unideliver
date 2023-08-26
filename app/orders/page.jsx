004b4d843067c65db4f2badfa3e35f983299771f
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-08-26 20:14:50 +0600

    redesigned order page

--- app/orders/page.jsx
+++ app/orders/page.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import Orders from '@/components/order/Orders'
+'use client'
 
-const OrderPage = () => {
+import React from 'react'
+import { useUserOrders } from '@/hooks'
+import Order from '@/components/order/Order'
+
+const Orders = () => {
+  const { orders, loading } = useUserOrders()
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
   return (
-    <div>
-      <Orders />
+    <div className="m-5 rounded-lg shadow-lg border-[1px] p-5">
+      <h2 className="border-b-2 text-2xl p-5">Your Orders</h2>
+      {orders.length > 0 ? (
+        orders.map((order) => <Order key={order.id} order={order} />)
+      ) : (
+        <p className="flex justify-center">No orders found.</p>
+      )}
     </div>
   )
 }
 
-export default OrderPage
+export default Orders
